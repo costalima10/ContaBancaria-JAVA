fcd8b5702c1d697f74cf5324e9de499ef2dbb873
costalima10 <gabrelcostalima10@gmail.com>
2023-11-17 14:44:53 -0300

    Adicionando cores, menu e model

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,21 +1,33 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
+import conta.util.Cores;
 
 public class Menu {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+    	
+    	// Teste da Classe Conta
+    			Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+    			c1.visualizar();
+    			c1.sacar(12000.0f);
+    			c1.visualizar();
+    			c1.depositar(5000.0f);
+    			c1.visualizar();
+        
+        Scanner leia = new Scanner(System.in);
+		
+		int opcao;
+				
+		while(true) {
 
-		Scanner leia = new Scanner(System.in);
-
-		int op;
-
-		while (true) {
-
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+			System.out.println("                                                     ");
+			System.out.println("                  BANCO DO GABRIEL                   ");
+			System.out.println("                                                     ");
 			System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||");
 			System.out.println("                                                     ");
-			System.out.println("                BANCO DO GABRIEL COSTA               ");
-			System.out.println("                                                     ");
-			System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||");
 			System.out.println("            1 - Criar Conta                          ");
 			System.out.println("            2 - Listar todas as Contas               ");
 			System.out.println("            3 - Buscar Conta por Numero              ");
@@ -25,64 +37,66 @@ public class Menu {
 			System.out.println("            7 - Depositar                            ");
 			System.out.println("            8 - Transferir valores entre Contas      ");
 			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     ");
 			System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||");
 			System.out.println("Entre com a opção desejada:                          ");
-			System.out.println("                                                     ");
-
-			op = leia.nextInt();
-
-			if (op == 9) {
-				System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+			
+			opcao = leia.nextInt();
+				
+			if (opcao == 9) {
+				System.out.println(Cores.TEXT_BLUE_BRIGHT + "\nBanco do Gabriel - O seu Futuro começa aqui!");
 				sobre();
-                 leia.close();
+				leia.close();
 				System.exit(0);
 			}
-
-			switch (op) {
+				
+			switch (opcao) {
 				case 1:
-					System.out.println("Criar Conta\n\n");
-
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
+				
+                    		break;
 				case 2:
-					System.out.println("Listar todas as Contas\n\n");
-
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
+					
+                    		break;
 				case 3:
-					System.out.println("Consultar dados da Conta - por número\n\n");
-
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
+	
+                    		break;
 				case 4:
-					System.out.println("Atualizar dados da Conta\n\n");
-
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
+					
+                    		break;
 				case 5:
-					System.out.println("Apagar a Conta\n\n");
-
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+		
+                   	 	break;
 				case 6:
-					System.out.println("Saque\n\n");
+					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
 
-					break;
+                   	 	break;
 				case 7:
-					System.out.println("Depósito\n\n");
-
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+					
+                    		break;
 				case 8:
-					System.out.println("Transferência entre Contas\n\n");
-
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
+					
+                    		break;
 				default:
-					System.out.println("\nOpção Inválida!\n");
-					break;
+					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+                    		break;
 			}
-		}
-	}
-    
-	public static void sobre() {
-		System.out.println("\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-		System.out.println("Projeto Desenvolvido por:Gabriel Costa ");
-		System.out.println("Gabriel Da Costa Lima - gabrielcostalima10@gmail.com");
-		System.out.println("https://github.com/costalima10/ContaBancaria-JAVA");
-		System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-	}
+		}	
+    }
+
+    public static void sobre() {
+	System.out.println("\n*********************************************************");
+	System.out.println("Projeto Desenvolvido por: ");
+	System.out.println("Generation Brasil - generation@generation.org");
+	System.out.println("github.com/conteudoGeneration");
+	System.out.println("*********************************************************");
+   }
+
 }
