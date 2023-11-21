a33ada2d98c096be6f93bf39eca26a533aa33d01
costalima10 <gabrelcostalima10@gmail.com>
2023-11-21 14:05:34 -0300

    Adicionando Classe corrente e poupança

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,11 +1,18 @@
 package conta;
 
 import java.util.Scanner;
+
 import conta.model.Conta;
 import conta.util.Cores;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
+	 
     public static void main(String[] args) {
+    	
+    	 Scanner leia = new Scanner(System.in);
+    	int opcao;		
     	
     	// Teste da Classe Conta
     			Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
@@ -14,11 +21,23 @@ public class Menu {
     			c1.visualizar();
     			c1.depositar(5000.0f);
     			c1.visualizar();
-        
-        Scanner leia = new Scanner(System.in);
-		
-		int opcao;
-				
+    			
+    	//Teste da Classe Conta Corrente
+    			ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
+    			cc1.visualizar();
+    			cc1.sacar(12000.0f);
+    			cc1.visualizar();
+    			cc1.depositar(5000.0f);
+    			cc1.visualizar();
+    			
+    	//Teste da Classe Conta Poupança
+    			ContaPoupanca cp1 = new ContaPoupanca(3, 123, 1, "Victor", 100000.0f, 15);
+    			cp1.visualizar();
+    			cp1.sacar(1000.0f);
+    			cp1.visualizar();
+    			cp1.depositar(5000.0f);
+    			cp1.visualizar();
+	
 		while(true) {
 
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
